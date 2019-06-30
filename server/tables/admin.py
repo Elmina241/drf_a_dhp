6d55a6a1f5599a0660543d29ccb30903d35c015d
Elmina Ross <Elmina Ross>
2019-07-01 09:02:29 +1000

    view

--- server/tables/admin.py
+++ server/tables/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Material
+from .models import *
 
 admin.site.register(Material)
+admin.site.register(Material_group)
+admin.site.register(Prefix)
+admin.site.register(Unit)
 
--- server/tables/views.py
+++ server/tables/views.py
@@ -1,45 +1,46 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import *
 from .serializers import *
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def material_list(request):
     if request.method == 'GET':
         materials = Material.objects.all()
         serializer = MaterialSerializer(materials, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = MaterialSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def material_detail(request, pk):
     try:
         material = Material.objects.get(pk=pk)
     except Material.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = MaterialSerializer(material)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
         serializer = MaterialSerializer(material, data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         material.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
