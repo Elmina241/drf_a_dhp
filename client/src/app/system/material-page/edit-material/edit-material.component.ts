2a19b785110270777ceb75d7353fc3cf47354d01
Elmina Ross <Elmina Ross>
2019-07-12 09:13:14 +1000

    edit

--- client/src/app/system/material-page/edit-material/edit-material.component.ts
+++ client/src/app/system/material-page/edit-material/edit-material.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Group, Material} from "../../shared/models/material.model";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-material',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditMaterialComponent implements OnInit {
 
+  @Input() groups: Array<Group>;
+  @Output() onMaterialEdit = new EventEmitter<Material>();
+
+  @Input() currentMaterialId = 1;
+  currentMaterial: Material;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm){
+
   }
 
 }
